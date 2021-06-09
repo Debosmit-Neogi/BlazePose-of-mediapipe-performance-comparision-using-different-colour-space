37ce85d3815bd7d794dd85c5c9f5e74e08f26feb
Debosmit Neogi <59002072+Debosmit-Neogi@users.noreply.github.com>
2021-06-09 13:04:13 +0530

    Update hsv.py

--- hsv.py
+++ hsv.py
@@ -1,32 +1,3 @@
-# import cv2
-# import mediapipe as mp
-# import time
-
-# cap=cv2.VideoCapture('/home/debosmit2001/Downloads/video.avi')
-
-# prev_time=0
-# while True:
-# 	success, img= cap.read()
-# 	imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     results = pose.process(imgRGB)
-#     # print(results.pose_landmarks)
-#     if results.pose_landmarks:
-#         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-#         for id, lm in enumerate(results.pose_landmarks.landmark):
-#             h, w, c = img.shape
-#             print(id, lm)
-#             cx, cy = int(lm.x * w), int(lm.y * h)
-#             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-
-# 	# find out frame rate of video
-# 	current_time= time.time()
-# 	fps=1/(current_time-prev_time)
-# 	prev_time=current_time  #reset the prev_time to current_time
-# 	# print(fps)
-# 	cv2.putText(img, str(int(fps)), (70,50),cv2.FONT_HERSHEY_PLAIN,3,(255,255,0),3)
-# 	cv2.imshow('img',img)
-# 	cv2.waitKey(10)
-
 
 import cv2
 import mediapipe as mp
